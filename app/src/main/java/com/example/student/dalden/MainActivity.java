3e4bebe3beb6eb54154f5fa98840b3e1875972a1
doamaster <derrickalden@gmail.com>
2017-07-18 10:30:50 -0700

    custom font

--- app/src/main/java/com/example/student/dalden/MainActivity.java
+++ app/src/main/java/com/example/student/dalden/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.student.dalden;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mTextView;
 
 
     @Override
@@ -16,7 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView mTextView = (TextView) findViewById(R.id.mainText);
+        //mTextView = (TextView) findViewById(R.id.mainText);
+
+        setup(savedInstanceState);
+
         mTextView.setText("Hello World");
 
         final Button button = (Button) findViewById(R.id.button1);
@@ -30,5 +35,19 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    protected void setup(Bundle savedInstanceState) {
+
+        //gets the mainFact textview frm the main activity
+        mTextView = (TextView) findViewById(R.id.mainText);
+
+        //sets up custom font from asset folder
+        Typeface mtypeFace = Typeface.createFromAsset(getAssets(),
+                "fonts/caveman.ttf");
+
+        mTextView.setTextSize(50);
+        mTextView.setTypeface(mtypeFace);
+
     }
 }
