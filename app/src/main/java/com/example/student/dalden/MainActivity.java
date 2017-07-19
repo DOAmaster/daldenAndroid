f6cbe393a6a9b768798932b32bc9f422f171d102
doa <jebus_for_life@yahoo.com>
2017-07-19 10:08:39 -0700

    added gold

--- app/src/main/java/com/example/student/dalden/MainActivity.java
+++ app/src/main/java/com/example/student/dalden/MainActivity.java
@@ -8,9 +8,19 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView mTextView;
+    TextView goldTotal;
+    TextView dimondTotal;
+    TextView gemTotal;
+    int gold = 0;
+    int dimond = 0;
+    int gem = 0;
+
+    Random rand = new Random();
 
 
     @Override
@@ -22,19 +32,64 @@ public class MainActivity extends AppCompatActivity {
 
         setup(savedInstanceState);
 
-        mTextView.setText("Hello World");
+        mTextView.setText("Gold Rush");
 
-        final Button button = (Button) findViewById(R.id.button1);
+        final Button button = (Button) findViewById(R.id.creditsButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
-                mTextView.setText("Derrick Alden WEW!");
+                //mTextView.setText("Derrick Alden");
 
-                Toast.makeText(getApplicationContext(), "DAlden", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Derrick Alden", Toast.LENGTH_SHORT).show();
 
             }
 
         });
+
+        final Button shopButton = (Button) findViewById(R.id.shopButton);
+        shopButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Coming Soon...", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+        final Button mineGoldButton = (Button) findViewById(R.id.collect);
+        mineGoldButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                goldTotal = (TextView) findViewById(R.id.goldText);
+                dimondTotal = (TextView) findViewById(R.id.dimondText);
+                gemTotal = (TextView) findViewById(R.id.gemText);
+
+                gold++;
+                goldTotal.setText(""+gold);
+
+                int n = rand.nextInt(100);
+                if (n == 1) {
+                    Toast.makeText(getApplicationContext(), "Dimond Collected", Toast.LENGTH_SHORT).show();
+                    dimond++;
+                    dimondTotal.setText("Dimonds: " + dimond);
+                }
+
+                int n2 = rand.nextInt(50);
+                if (n2 == 1) {
+                    Toast.makeText(getApplicationContext(), "Gem Collected", Toast.LENGTH_SHORT).show();
+                    gem++;
+                    gemTotal.setText("Gems: " + gem);
+                }
+
+
+
+                //Toast.makeText(getApplicationContext(), "Gold Collected", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+
     }
 
     protected void setup(Bundle savedInstanceState) {
